7c8edf02e1abe930a81ef2429ca8c6b8a477dd3d
Tarun Singh LNMIIT <thetseffect@gmail.com>
2020-06-01 21:04:46 +0530

    Removed dotenv

--- client/src/config/keys.js
+++ client/src/config/keys.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === "production") {
   module.exports = {
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
     MONGODB_URI: process.env.MONGODB_URI,
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_ID: process.env.REACT_APP_GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
   };
 } else {
